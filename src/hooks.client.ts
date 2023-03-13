6653a80e5310fa7fcfc07eb781c2c8c81425a72b
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2023-03-12 22:29:00 -0500

    style: handle errors in a basic capacity

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,2 +1,10 @@
 // Ensures the supabase client is initialized on the server and client -> hooks.server.ts
 import '$lib/supabaseClient'
+
+export const handleError = (error: Error) => {
+  console.error('client error:', error)
+  return {
+    status: 500,
+    body: error.message
+  }
+}
