0049c3facb269e84a7d641fe26e8ad2d7f839ebf
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2023-03-12 23:43:48 -0500

    feat: add zod to signin

--- src/routes/(auth)/signin/+page.svelte
+++ src/routes/(auth)/signin/+page.svelte
@@ -1,56 +1,71 @@
 <script lang="ts">
-  import type { ActionData } from './$types'
-  import { enhance } from '$app/forms'
-  import { faWarning } from '@fortawesome/free-solid-svg-icons'
-  import Fa from 'svelte-fa'
+	import { enhance } from '$app/forms'
+	import Alert from '$lib/components/Alert.svelte'
+	import type { ActionData } from './$types'
+	import type { Snapshot } from './$types'
 
-  export let form: ActionData
-  let loading = false
+	export let form: ActionData
+	let loading = false
+
+	let formElement: HTMLFormElement
+
+	// Set email to form.email if it exists. This is a workaround for the persisting email value when the username has an error.
+	let email = ''
+	$: if (form?.email) {
+		email = form.email
+	}
+
+	export const snapshot: Snapshot = {
+		capture: () => {
+			const data = new FormData(formElement)
+			data.set('password', '')
+			return Object.fromEntries(data)
+		},
+		restore: (value) => {
+			for (const control of formElement.elements) {
+				const name = control.getAttribute('name')
+				if (name) {
+					const inputControl = control as HTMLFormElement
+					inputControl.value = value[name] as string
+				}
+			}
+		}
+	}
 </script>
 
 <section class="max-w-sm mx-auto">
-  <div class="prose">
-    <h1>Sign In</h1>
-  </div>
-  <form action="?/signin" method="POST" use:enhance class="flex flex-col gap-6 my-6">
-    {#if form?.error}
-      <div class="alert alert-error">
-        <div>
-          <Fa icon={faWarning} />
-          {form.error}
-        </div>
-      </div>
-    {/if}
-    <p>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email..."
-        class="input input-bordered w-full"
-        required
-        value={form?.values?.email ?? ''}
-      />
-    </p>
-    <p>
-      <input
-        type="password"
-        name="password"
-        placeholder="Password..."
-        class="input input-bordered w-full"
-        required
-      />
-    </p>
-    <p class="flex items-center gap-6 mt-6">
-      <button disabled={loading} class="btn btn-primary">Sign In</button>
-      or
-      <a href="/signup" class="link">Sign up</a>
-    </p>
-  </form>
-
-  {#if form}
-    <section class="my-8">
-      <h3>Form data:</h3>
-      <pre>{JSON.stringify(form, null, 2)}</pre>
-    </section>
-  {/if}
+	<div class="prose">
+		<h1>Sign In</h1>
+	</div>
+	<form method="POST" class="flex flex-col gap-1 my-6" bind:this={formElement} use:enhance>
+		{#if form?.message !== undefined || form?.errors}
+			<Alert class="alert-error mb-6">{'Email or Password is incorrect.'}</Alert>
+		{/if}
+		<div class="form-control">
+			<input
+				type="email"
+				name="email"
+				placeholder="Email..."
+				bind:value={email}
+				class="input input-bordered w-full {form?.message !== undefined || form?.errors
+					? 'input-error'
+					: 'input-bordered'}"
+			/>
+		</div>
+		<div class="form-control mt-5">
+			<input
+				type="password"
+				name="password"
+				placeholder="Password..."
+				class="input input-bordered w-full {form?.message !== undefined || form?.errors
+					? 'input-error'
+					: 'input-bordered'}"
+			/>
+		</div>
+		<p class="flex items-center gap-6 mt-9">
+			<button disabled={loading} class="btn btn-primary">Sign In</button>
+			or
+			<a href="/signup" class="link">Sign up</a>
+		</p>
+	</form>
 </section>
