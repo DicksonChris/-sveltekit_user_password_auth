211d125cb8fff8fc9b2421beb97bf3729e137318
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2023-03-12 22:40:18 -0500

    refactor: comment terms (tos) temp page

--- src/routes/(auth)/signup/terms/+page.svelte
+++ src/routes/(auth)/signup/terms/+page.svelte
@@ -1,1 +1,8 @@
-<p class='text-center'>TERMS AND CONDTIONS</p>
+<script>
+  import { redirect } from '@sveltejs/kit'
+</script>
+
+<!-- Acknowledgement of TOS is not saved to server. If TOS will be frequently updated then save the Date the TOS was agreed to. Else you must send TOS updates to all users -->
+
+<!-- button to navigate back to signup -->
+<p class="text-center">TERMS AND CONDTIONS</p>
