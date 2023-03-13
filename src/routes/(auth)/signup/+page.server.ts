b0293e24447cf0333e89f9e5a2c5867dd6921f30
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2023-03-12 23:45:47 -0500

    feat: implements Zod in signup, change email redirect

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -5,45 +5,46 @@ import { fail } from '@sveltejs/kit'
 import { ZodError } from 'zod'
 import type { Actions } from './$types'
 
-// Define the available actions for this endpoint
 export const actions: Actions = {
-  register: async ({ request, locals, url }) => {
-    const formData = await request.formData()
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const passwordConfirm = formData.get('passwordConfirm') as string
-    const terms = formData.get('terms') as string
+	register: async ({ request, locals, url }) => {
+		const formData = await request.formData()
+		const email = formData.get('email') as string
+		const password = formData.get('password') as string
+		const passwordConfirm = formData.get('passwordConfirm') as string
+		const terms = formData.get('terms') as string
 
-    // Use zod to validate the form data
-    try {
-      signUpSchema.parse({ email, password, passwordConfirm, terms })
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const errors = formatError(err)
-        return fail(400, { errors, email })
-      }
-    }
+		// Use zod to validate the form data
+		try {
+			signUpSchema.parse({ email, password, passwordConfirm, terms })
+		} catch (err) {
+			if (err instanceof ZodError) {
+				const errors = formatError(err)
+				return fail(400, { errors, email })
+			}
+		}
 
-    // Attempt to sign up the user
-    const credentials = {
-      email,
-      password,
-      options: { emailRedirectTo: url.origin },
-    }
+		// Attempt to sign up the user
+		const credentials = {
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${url.origin}/profile` // necessary for our playwright test
+			}
+		}
 
-    // Rename error to avoid conflict with the sveltekit's error
-    const { error: err } = await locals.sb.auth.signUp(credentials)
+		// Rename error to avoid conflict with the sveltekit's error
+		const { error: err } = await locals.sb.auth.signUp(credentials)
 
-    // If the error is a 400 error, return a 400 response with an error message.
-    if (err) {
-      if (err instanceof AuthApiError && err.status === 400) {
-        return fail(400, faultResponse('Invalid credentials.', { email }))
-      }
+		// If the error is a 400 error, return a 400 response with an error message.
+		if (err) {
+			if (err instanceof AuthApiError && err.status === 400) {
+				return fail(400, faultResponse('Invalid credentials.', { email }))
+			}
 
-      // If the error is not a 400 error, return a 500 response with an error message.
-      return fail(500, faultResponse(err.message, { email }))
-    }
+			// If the error is not a 400 error, return a 500 response with an error message.
+			return fail(500, faultResponse(err.message, { email }))
+		}
 
-    return successResponse('Success! Check your email for a confirmation link.')
-  },
+		return successResponse('Success! Check your email for a confirmation link.')
+	}
 }
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,103 +1,107 @@
 <script lang="ts">
-  import { enhance } from '$app/forms'
-  import InputErrorAlert from '$lib/components/InputErrorAlert.svelte'
-  import type { Snapshot } from './$types'
-  import type { ActionData } from './$types'
+	import { enhance } from '$app/forms'
+	import Alert from '$lib/components/Alert.svelte'
+	import InputErrorAlert from '$lib/components/InputErrorAlert.svelte'
+	import type { Snapshot } from './$types'
+	import type { ActionData } from './$types'
 
-  export let form: ActionData
-  let loading = false
+	export let form: ActionData
+	let loading = false
 
-  let formElement: HTMLFormElement
+	let formElement: HTMLFormElement
 
-  export const snapshot: Snapshot = {
-    capture: () => {
-      const data = new FormData(formElement)
-      return Object.fromEntries(data)
-    },
-    restore: (value) => {
-      for (const control of formElement.elements) {
-        const name = control.getAttribute('name')
-        if (name) {
-          const inputControl = control as HTMLInputElement // Type assertion
-          inputControl.value = value[name] as string
-        }
-      }
-    },
-  }
+	// Set email to form.email if it exists. This is a workaround for the persisting email value when the username has an error
+	let email = ''
+	$: if (form?.email) {
+		email = form.email
+	}
+
+	export const snapshot: Snapshot = {
+		capture: () => {
+			const data = new FormData(formElement)
+			return Object.fromEntries(data)
+		},
+		restore: (value) => {
+			for (const control of formElement.elements) {
+				const name = control.getAttribute('name')
+				if (name) {
+					const inputControl = control as HTMLInputElement // Type assertion
+					inputControl.value = value[name] as string
+				}
+			}
+		}
+	}
 </script>
 
 <section class="max-w-sm mx-auto">
-  <div class="prose">
-    <h1>Sign Up</h1>
-  </div>
-  <form
-    action="?/register"
-    method="POST"
-    class="flex flex-col gap-1 my-6"
-    bind:this={formElement}
-    use:enhance
-  >
-    <div class="form-control">
-      <input
-        type="email"
-        name="email"
-        placeholder="Email..."
-        class="input input-bordered w-full {form?.errors?.email ? 'input-error' : 'input-bordered'}"
-      />
-      <InputErrorAlert formErrorName="email" formErrorData={form?.errors?.email} />
-    </div>
-    <div class="form-control">
-      <input
-        type="password"
-        name="password"
-        placeholder="Password..."
-        class="input input-bordered w-full {form?.errors?.password
-          ? 'input-error'
-          : 'input-bordered'}"
-      />
-      <InputErrorAlert formErrorName="password" formErrorData={form?.errors?.password} />
-    </div>
-    <div class="form-control">
-      <input
-        type="password"
-        name="passwordConfirm"
-        placeholder="Confirm password..."
-        class="input input-bordered w-full {form?.errors?.passwordConfirm
-          ? 'input-error'
-          : 'input-bordered'}"
-      />
-      <InputErrorAlert
-        formErrorName="passwordConfirm"
-        formErrorData={form?.errors?.passwordConfirm}
-      />
-    </div>
-    <div class="form-control">
-      <label for="terms" class="flex items-center gap-4">
-        <input
-          type="checkbox"
-          name="terms"
-          id="terms"
-          class="checkbox checkbox-primary {form?.errors?.terms ? 'border-error' : ''}"
-        />
-        <span
-          >I accept the
+	<div class="prose">
+		<h1>Sign Up</h1>
+	</div>
+	<form
+		action="?/register"
+		method="POST"
+		class="flex flex-col gap-1 my-6"
+		bind:this={formElement}
+		use:enhance
+	>
+		{#if form?.message !== undefined}
+			<Alert class="{form?.success ? 'alert-info' : 'alert-error'} mb-6">{form?.message}</Alert>
+		{/if}
+		<div class="form-control">
+			<input
+				type="email"
+				name="email"
+				placeholder="Email..."
+				bind:value={email}
+				class="input input-bordered w-full {form?.errors?.email ? 'input-error' : 'input-bordered'}"
+			/>
+			<InputErrorAlert formErrorName="email" formErrorData={form?.errors?.email} />
+		</div>
+		<div class="form-control">
+			<input
+				type="password"
+				name="password"
+				placeholder="Password..."
+				class="input input-bordered w-full {form?.errors?.password
+					? 'input-error'
+					: 'input-bordered'}"
+			/>
+			<InputErrorAlert formErrorName="password" formErrorData={form?.errors?.password} />
+		</div>
+		<div class="form-control">
+			<input
+				type="password"
+				name="passwordConfirm"
+				placeholder="Confirm password..."
+				class="input input-bordered w-full {form?.errors?.passwordConfirm
+					? 'input-error'
+					: 'input-bordered'}"
+			/>
+			<InputErrorAlert
+				formErrorName="passwordConfirm"
+				formErrorData={form?.errors?.passwordConfirm}
+			/>
+		</div>
+		<div class="form-control">
+			<label for="terms" class="flex items-center gap-4">
+				<input
+					type="checkbox"
+					name="terms"
+					id="terms"
+					class="checkbox checkbox-primary {form?.errors?.terms ? 'border-error' : ''}"
+				/>
+				<span
+					>I accept the
 
-          <a href="/signup/terms" class="link link-primary">Terms and Conditions</a></span
-        >
-      </label>
-      <InputErrorAlert formErrorName="terms" formErrorData={form?.errors?.terms} />
-    </div>
-    <p class="flex items-center gap-6 mt-6">
-      <button disabled={loading} class="btn btn-primary">Sign Up</button>
-      or
-      <a href="/signin" class="link">Sign In</a>
-    </p>
-  </form>
-
-  {#if form}
-    <section class="my-8">
-      <h3>Form data:</h3>
-      <pre>{JSON.stringify(form, null, 2)}</pre>
-    </section>
-  {/if}
+					<a href="/signup/terms" class="link link-primary">Terms and Conditions</a></span
+				>
+			</label>
+			<InputErrorAlert formErrorName="terms" formErrorData={form?.errors?.terms} />
+		</div>
+		<p class="flex items-center gap-6 mt-6">
+			<button disabled={loading} class="btn btn-primary">Sign Up</button>
+			or
+			<a href="/signin" class="link">Sign In</a>
+		</p>
+	</form>
 </section>
